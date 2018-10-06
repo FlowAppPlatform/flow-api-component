c7203da2344b8d9c01372ff3b1203e7c2f83cfaa
Jerry Edebua <jeredebua@gmail.com>
2018-10-06 10:11:40 +0300

    removed emitResult function

--- src/post.js
+++ src/post.js
@@ -8,25 +8,26 @@ module.exports = class PostComponent extends APIComponent {
 
     // we make request here
     this.attachTask(function () {
-      try {
-
-        new Axios(
-          this.getProperty('URL').data,
-          this.getProperty('Headers').data
-        )
-          .request('post', this.getProperty('Data').data)
-          .then(response => {
-            this.emitResult('Complete', response.data);
-          })
-          .catch(err => {
-            this.emitResult('Failed');
-            throw err;
-          });
-
-      } catch(error) {
-        this.emitResult('Failed');
-        throw error;
-      }
+      
+      new Axios(
+        this.getProperty('URL').data,
+        this.getProperty('Headers').data
+      )
+        .request('post', this.getProperty('Data').data)
+        .then(response => {
+          const port = this.getPort('Complete');
+          port.getProperty('Data').data = response.data;
+          port.emit();
+          this.taskComplete();
+        })
+        .catch(err => {
+          const port = this.getPort('Failed');
+          port.getProperty('Data').data = err;
+          port.emit();
+          this.taskComplete();
+          throw err;
+        });
+    
     });
 
   }
