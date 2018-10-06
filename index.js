b19207a093ffe5b927a067cf0377c4366ec63587
Jerry Edebua <jeredebua@gmail.com>
2018-10-06 10:49:57 +0300

    added index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+import GET from './src/get';
+import POST from './src/post';
+import PUT from './src/put';
+import DELETE from './src/delete';
+
+module.exports = {
+  GET,
+  POST,
+  PUT,
+  DELETE
+};
